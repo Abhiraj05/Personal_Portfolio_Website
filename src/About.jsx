a8d48ba385e5109c0fd769fd0d4d3694c7de5617
Abhiraj Shilkar <93819088+Abhiraj05@users.noreply.github.com>
2024-11-12 13:25:40 +0530

    Update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -10,7 +10,7 @@ function About(){
             <p className='abt-txt'>about me</p>
             <div>
 
-    <p className="text-transparent bg-clip-text bg-gradient-to-br from-blue-500 to-white text-justify"> Hello !
+    <p className="text-transparent bg-clip-text bg-gradient-to-br from-blue-500 to-white text-center"> Hello !
                  I'm Abhiraj Shilkar, an aspiring full-stack developer currently pursuing a degree in information technology at Goa College of Engineering. 
                 I have a keen interest in web3, blockchain,  
                    artificial intelligence & 
