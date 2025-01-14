73944314df13d5ab2729af907b6e991f0dbf97a2
Abhiraj Shilkar <93819088+Abhiraj05@users.noreply.github.com>
2025-01-14 20:30:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -82,7 +82,7 @@ function App() {
         </div>
         <div className="text-center">
           <button className="resume-btn bg-gradient-to-t from-blue-500 to-slate-300 shadow-blue-400 hover:opacity-70 ">
-            <a href="https://drive.google.com/file/d/1F0gICgdaoH3YD5w5Uy0kW9-QjlvkkO7d/view?usp=sharing">
+            <a href="https://drive.google.com/file/d/1fI3nS7TQ_luR5suBscFF30E01t-5-AwM/view?usp=sharing">
               resume
             </a>
           </button>
